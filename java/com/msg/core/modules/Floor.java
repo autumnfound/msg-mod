de461b86a7af8bc59ca795d40a979d530fd73deb
autumnfound <mtlowe@live.ca>
2016-10-27 20:45:19 -0400

    27/10 change commit
    +added stairwell methods for multi-floor buildings.

--- java/com/msg/core/modules/Floor.java
+++ java/com/msg/core/modules/Floor.java
@@ -1,6 +1,7 @@
 package com.msg.core.modules;
 
 import com.msg.core.util.BlockData;
+import com.msg.core.util.BlockUtil;
 
 public class Floor {
 	/**
@@ -17,7 +18,7 @@ public class Floor {
 	public static BlockData[][] gen(int xWidth, int zWidth, int[] style) {
 		BlockData[][] floor = new BlockData[xWidth][zWidth];
 		// create the outer layer for the floor layer
-		
+
 		// -1 is handled by generator to preserve the existing block in that
 		// space.
 		BlockData[] outerLayer = new BlockData[zWidth];
@@ -31,18 +32,59 @@ public class Floor {
 			int styleSet = (xOffset == 1 || xOffset == xWidth - 2) ? 0 : 1;
 			// write non-writing block around edge of layer.
 			floor[xOffset][0] = new BlockData(-1, -1);
-			
+
 			floor[xOffset][1] = new BlockData(style[styleSet], -1);
 			for (int zOffset = 2; zOffset < zWidth - 2; zOffset++) {
 				floor[xOffset][zOffset] = new BlockData(style[styleSet + 1], -1);
 			}
 			floor[xOffset][zWidth - 2] = new BlockData(style[styleSet], -1);
-			
+
 			// write non-writing block around edge of layer.
 			floor[xOffset][zWidth - 1] = new BlockData(-1, -1);
 		}
 		floor[xWidth - 1] = outerLayer;
 
 		return floor;
+	}
+
+	public static BlockData[][] generateStairwell(BlockData[][] currentFloor, int[] style) {
+		// get the current room width's
+		int xWidth = currentFloor.length;
+		int zWidth = currentFloor[0].length;
+
+		boolean isXWidest = xWidth >= zWidth;
+
+		// set offsets according to which side is the longest, and gets stair
+		// direction.
+		int xOffset = 2;
+		int zOffset = 2;
+		int stairMetadata;
+
+		// set metadata for which direction stairs should face.
+		if (isXWidest) {
+			stairMetadata = BlockUtil.META_STAIR_WEST_FACING;
+		} else {
+			stairMetadata = BlockUtil.META_STAIR_NORTH_FACING;
+		}
+		
+		// loop through the floor and remove blocks for a stairwell
+		for (int i = 3; i < (isXWidest ? xWidth : zWidth) - 3; i++) {
+			currentFloor[xOffset][zOffset].setBlockID(-1);
+			currentFloor[xOffset][zOffset].setMetadata(-1);
+			
+			// increment offset depending on which wall is being written to
+			if (isXWidest) {
+				xOffset++;
+			} else {
+				zOffset++;
+			}
+		}
+		
+		// set stair block to finish stair case.
+		currentFloor[xOffset][zOffset].setBlockID(style[0]);
+		currentFloor[xOffset][zOffset].setMetadata(stairMetadata);
+		currentFloor[xOffset][zOffset].setStairs(true);
+
+		return currentFloor;
 	}
 }
